fc8f2162241e0d54451e011150d81880d7caf463
diana-stupak <147924515+diana-stupak@users.noreply.github.com>
2025-06-05 00:19:01 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const assetUrls = ['index.html', 'script.js', 'style.css']; // Масив ресурсів, які будуть кешовані
-const staticCacheName = 's-app-v1'; // Назва кешу статичних файлів
+const assetUrls = ['index.html', 'script.js', 'style.css']; 
+const staticCacheName = 's-app-v1'; 
 
-self.addEventListener('install', event => { // Додати обробник події "install" для Service Worker
-    console.log('[SW]: install'); // Вивести повідомлення у консоль під час інсталяції Service Worker
-    event.waitUntil( // Очікувати завершення дії, перед тим як закінчити обробку події
-        caches.open(staticCacheName).then(cache => cache.addAll(assetUrls)) // Відкрити кеш і додати всі ресурси з масиву assetUrls в кеш
+self.addEventListener('install', event => { 
+    console.log('[SW]: install'); 
+    event.waitUntil( 
+        caches.open(staticCacheName).then(cache => cache.addAll(assetUrls)) 
     );
 });
 
-self.addEventListener('activate', event => { // Додати обробник події "activate" для Service Worker
-    console.log('[SW]: activate'); // Вивести повідомлення у консоль під час активації Service Worker
+self.addEventListener('activate', event => { 
+    console.log('[SW]: activate'); 
 });
